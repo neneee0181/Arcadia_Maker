730e71157277c1f0b10d42bcc3bde420c5e13aec
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-14 19:06:47 +0900

    update : main #4

--- main.py
+++ main.py
@@ -1,47 +1,8 @@
-from pico2d import *
+from pico2d import open_canvas, delay, close_canvas
+import game_framework
 
-from src.config.config import screen_width, screen_height
-from src.start_background import StartBackground
+import play_mode as start_mode
 
-def handle_events():
-    global running
-
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-
-def reset_world():
-    global running
-    global background
-    global world
-
-    running = True
-    world = []
-
-    background = StartBackground()
-    world.append(background)
-
-def update_world():
-    for o in world:
-        o.update()
-    pass
-
-def render_world():
-    clear_canvas()
-    for o in world:
-        o.draw()
-    update_canvas()
-
-open_canvas(screen_width, screen_height)
-reset_world()
-# game loop
-while running:
-    handle_events()
-    update_world()
-    render_world()
-    delay(0.01)
-# finalization code
+open_canvas(1600, 600)
+game_framework.run(start_mode)
 close_canvas()
