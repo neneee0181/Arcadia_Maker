07632a18d104122ed760e8bcb07e59289779170c
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-14 23:32:35 +0900

    update : 전체화면 모드 적용

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pico2d import open_canvas, delay, close_canvas
 import src.config.game_framework as game_framework
 import src.logo_mode as start_mode
+import src.config.config as config
 
-open_canvas(1600, 600)
+open_canvas(config.screen_width, config.screen_height)
 game_framework.run(start_mode)
 close_canvas()
