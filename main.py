aebb173220f26630896872c393d9869b9fa22e12
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-14 21:31:31 +0900

    feat : game_framework add #4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import open_canvas, delay, close_canvas
-import game_framework
+import src.
 
-import play_mode as start_mode
+import src.config.play_mode as start_mode
 
 open_canvas(1600, 600)
 game_framework.run(start_mode)
