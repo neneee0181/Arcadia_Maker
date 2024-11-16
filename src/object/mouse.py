376d214e3fcc864406bfcdc325afbd95c4af93d4
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-17 02:20:27 +0900

    update : 마우스 클릭 상태에 따라 마우스 커서 이미지 변경 #3

--- src/object/mouse.py
+++ src/object/mouse.py
@@ -2,15 +2,21 @@ from pico2d import *
 
 
 class Mouse:
-    image = None
+    image_down = None
+    image_up = None
+    click_status = False
 
     def __init__(self, x=-100, y=-100, velocity=1):
-        if Mouse.image == None:
-            Mouse.image = load_image('./src/asset/button/hand_open.png')
+        if Mouse.image_down == None:
+            Mouse.image_down = load_image('./src/asset/button/hand_closed.png')
+            Mouse.image_up = load_image('./src/asset/button/hand_open.png')
         self.x, self.y, self.velocity = x, y, velocity
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        if self.click_status:
+            self.image_down.draw(self.x, self.y)
+        else:
+            self.image_up.draw(self.x, self.y)
         draw_rectangle(*self.get_bb())
 
     def handle_event(self, event, tiles):
@@ -18,6 +24,9 @@ class Mouse:
             self.x, self.y = event.x, get_canvas_height() - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
             print(event.x, event.y)
+            self.click_status = True
+        elif event.type == SDL_MOUSEBUTTONUP:
+            self.click_status = False
 
     pass
 
@@ -26,7 +35,7 @@ class Mouse:
 
     def get_bb(self):
         # fill here
-        return self.x - self.image.w // 2, self.y - self.image.w // 2, self.x + self.image.w // 2, self.y + self.image.w // 2
+        return self.x - self.image_down.w // 2, self.y - self.image_down.w // 2, self.x + self.image_down.w // 2, self.y + self.image_down.w // 2
         pass
 
     def handle_collision(self, group, other):
