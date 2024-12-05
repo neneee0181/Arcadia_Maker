979f2bc09253ba637399d21b7e96df9811ac6bb6
neneee <2022180024@tukorea.ac.kr>
2024-12-05 16:03:51 +0900

    update : 창모드로 변경 #3

--- src/config/config.py
+++ src/config/config.py
@@ -8,6 +8,8 @@ effect_sound_size = 20
 
 # 전체 화면
 root = tk.Tk()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+# screen_width = root.winfo_screenwidth()
+# screen_height = root.winfo_screenheight()
+screen_width = 1280
+screen_height = 680
 root.destroy()
